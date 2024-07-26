ac033b941c799931eadd851451d5dc9b384fef86
yunn31425 <odu3971@gmail.com>
2024-07-26 11:50:07 +0900

    20240726

--- server/app.py
+++ server/app.py
@@ -4,7 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cube_data = [
+        {
+            'position': [128.09964, 35.1532],  # Longitude, Latitude
+            'altitude': 0,
+            'rotation': [0, 0, 0],  # Rotation angles in radians
+            'size': 10,  # Size of the cube
+            'image': 'your_image.png'  # Image file name
+        },
+        {
+            'position': [128.09994, 35.1532],  # Longitude, Latitude
+            'altitude': 0,
+            'rotation': [1.052, 0, 0],  # Rotation angles in radians
+            'size': 10,  # Size of the cube
+            'image': 'your_image.png'  # Image file name
+        }
+    ]
+    
+    return render_template('index.html', cube_data=cube_data,
+                           mapbox_access_token='pk.eyJ1IjoieXVubiIsImEiOiJjbHR3dDYyMXAwMzR0MmtwNHo1bWk3dW1qIn0.o4nBVu9qWb5Hl13LZtebzA')
 
 if __name__ == '__main__':
     app.run(debug=True)
