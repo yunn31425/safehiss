99d57fd38db5c9d74952334d7500925874a18257
yunn31425 <odu3971@gmail.com>
2024-08-06 00:24:30 +0900

    drone 추가

--- server/app.py
+++ server/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request
 from flask_socketio import disconnect, emit, SocketIO
+import random, time, eventlet
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
-
-# @app.before_request() # 처음 사용자 접속시
 
 @app.route('/')
 def index():
+    print(1)
     cube_data = [
         {
             'position': [128.09964, 35.1532],  # Longitude, Latitude
@@ -43,9 +44,28 @@ def index():
     return render_template('index.html', cube_data=cube_data,
                            mapbox_access_token='pk.eyJ1IjoieXVubiIsImEiOiJjbHR3dDYyMXAwMzR0MmtwNHo1bWk3dW1qIn0.o4nBVu9qWb5Hl13LZtebzA')
 
+def get_random_location():
+    return {
+                "droneID" : 1,
+                "lng" : 128.09967 + round(random.randrange(-9,9)*0.00001, 5), 
+                "lat" : 35.1532 + round(random.randrange(-9,9)*0.00001, 5),
+            }
 
-# @socketio.on('connect')
-# def 
+@socketio.on('connect')
+def connected():
+    print("클라이언트 연결됨")
+    
+    def update_Location():
+        while True:
+            print("새 위치 전송 중")
+            socketio.emit('droneLocation', get_random_location())
+            eventlet.sleep(1)
+    
+    socketio.start_background_task(target=update_Location)
+
+@socketio.on('disconnect')
+def disconnected():
+    print("클라이언트 연결 해제됨")
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
